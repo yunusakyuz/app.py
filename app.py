7e0b3fa6d17c8fb6c4004f5714231515d3a92bc0
yunusakyuz <66246189+yunusakyuz@users.noreply.github.com>
2020-09-01 22:57:10 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def head():
+    return "Hello World."
+
+@app.route("/second")
+def second():
+    return "This is the second page."
+
+@app.route("/third/subthird")
+def third():
+    return "This is the subthird page."
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"ID of this page is {id}"
+
+if __name__ == "__main__" :
+    app.run(debug = True)  #Bize hatalari verecek.
